00a21babdcedb72eefa87a4f4ff2a9c81a26e5ae
hjj <hejiaojiao0302@163.com>
2023-03-10 00:11:34 +0800

    新增图片上传

--- health_parent/health_backend/src/main/java/com/health/controller/SetmealController.java
+++ health_parent/health_backend/src/main/java/com/health/controller/SetmealController.java
@@ -2,13 +2,20 @@ package com.health.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.health.constant.MessageConstant;
+import com.health.constant.RedisConstant;
 import com.health.entity.Setmeal;
 import com.health.service.SetmealService;
+import com.health.util.QiniuUtils;
 import com.health.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import redis.clients.jedis.JedisPool;
+
+import java.util.UUID;
 
 
 @RestController
@@ -16,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetmealController {
     @Reference
     private SetmealService setmealService;
+    @Autowired
+    private JedisPool jedisPool;
     /**
      * @Description: 新增套餐成功
      * @author: hjj
@@ -32,6 +41,25 @@ public class SetmealController {
         } catch (Exception e) {
             e.printStackTrace();
             return new Result(false,MessageConstant.ADD_SETMEAL_FAIL);
+        }
+    }
+    @RequestMapping("/upload")
+    public Result upload(@RequestParam("imgFile")MultipartFile imgFile){
+        //获取原始文件名
+        String originalFilename = imgFile.getOriginalFilename();
+        //为避免图片名称重复，给图片生成新的名称，并加上原来的后缀名
+        int index = originalFilename.lastIndexOf(".");
+        String suffix = originalFilename.substring(index);
+        String fileName = UUID.randomUUID().toString() + suffix;
+        //将图片保存到七牛云
+        try {
+            QiniuUtils.upload2Qiniu(imgFile.getBytes(),fileName);
+            //文件上传成功后，将文件保存在redis中
+            jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_RESOURCES,fileName);
+            return new Result(true, MessageConstant.PIC_UPLOAD_SUCCESS,fileName);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.PIC_UPLOAD_FAIL);
         }
     }
 }
--- health_parent/health_service_provider/src/main/java/com/health/service/SetmealServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/health/service/SetmealServiceImpl.java
@@ -1,10 +1,12 @@
 package com.health.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.health.constant.RedisConstant;
 import com.health.dao.SetmealDao;
 import com.health.entity.Setmeal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.JedisPool;
 
 @Service(interfaceClass = SetmealService.class)
 @Transactional
@@ -12,6 +14,8 @@ public class SetmealServiceImpl implements SetmealService{
 
     @Autowired
     private SetmealDao setmealDao;
+    @Autowired
+    private JedisPool jedisPool;
 
     @Override
     public void add(Integer[] checkgroupIds, Setmeal setmeal) {
@@ -21,5 +25,7 @@ public class SetmealServiceImpl implements SetmealService{
         for (Integer checkgroupId:checkgroupIds){
             setmealDao.addSetmealAndCheckgroup(setmeal.getId(),checkgroupId);
         }
+        //完成数据库操作后需要将图片名称保存到redis
+        jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_DB_RESOURCES,setmeal.getImg());
     }
 }
