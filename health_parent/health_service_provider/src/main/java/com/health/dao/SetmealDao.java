f76671b21e542b24b7b95e822fbcebf45a2c7973
hjj <hejiaojiao0302@163.com>
2023-03-11 21:29:41 +0800

    修改无法上传图片到redis服务器的原因

--- health_parent/health_service_provider/src/main/java/com/health/dao/SetmealDao.java
+++ health_parent/health_service_provider/src/main/java/com/health/dao/SetmealDao.java
@@ -1,9 +1,10 @@
 package com.health.dao;
 
 import com.health.entity.Setmeal;
+import org.apache.ibatis.annotations.Param;
 
 public interface SetmealDao {
     void add(Setmeal setmeal);
 
-    void addSetmealAndCheckgroup(Integer id, Integer checkgroupId);
+    void addSetmealAndCheckgroup(@Param("setmeal_id") Integer id, @Param("checkgroup_id") Integer checkgroupId);
 }
